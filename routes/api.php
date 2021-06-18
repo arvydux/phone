96d2b57989a638940c37601d7252862f64dac749
Arvydas Kavaliauskas <arvydas.kavaliauskas83@gmail.com>
2021-06-18 12:04:33 +0300

    Passport Auth package installed and configured for use

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PassportAuthController;
+
+
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+
+});
 
 /*
 |--------------------------------------------------------------------------
